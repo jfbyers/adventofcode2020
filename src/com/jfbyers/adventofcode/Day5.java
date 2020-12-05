389bf5b82d364d67760481455eb21b7c367d14e9
Eduardo Aguado <aguado@gmail.com>
2020-12-05 17:10:31 +0100

    Day 5

---
+++ src/com/jfbyers/adventofcode/Day5.java
@@ -0,0 +1,84 @@
+package com.jfbyers.adventofcode;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day5 {
+
+
+    private final static List<BoardingPass> passes = getBoardingPasses();
+
+    public static void main(String[] args) {
+        List<Integer> seatIds = getSeatIds();
+        int max = Collections.max(seatIds);
+        System.out.println("Maximum id is : "+max);
+        Collections.sort(seatIds);
+        int mySeatId = getMySeatId(seatIds);
+        System.out.println("My seat  id is : "+mySeatId);
+
+
+    }
+
+    private static int getMySeatId(List<Integer> seatIds) {
+        int previousId = seatIds.get(0) -1 ;
+        for (int i = 0; i< seatIds.size(); i++){
+            int id = seatIds.get(i);
+            if (id != previousId + 1) {
+               return (previousId +  1);
+            }
+            previousId = id;
+        }
+        return -1;
+    }
+
+    private static List<Integer> getSeatIds() {
+        List<Integer> seatIds = new ArrayList<>();
+        for (BoardingPass p : passes){
+            int seatId = p.row * 8 + p.seat;
+            seatIds.add(seatId);
+        }
+        return seatIds;
+    }
+
+
+    private static List<BoardingPass> getBoardingPasses() {
+        final File file = new File("inputDay5.txt");
+        List<BoardingPass> boardingPasses = new ArrayList<>();
+        try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                BoardingPass pass = BoardingPass.fromString(line);
+                boardingPasses.add(pass);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return boardingPasses;
+    }
+
+    private static class BoardingPass {
+
+        private final int row;
+        private final int seat;
+
+        public BoardingPass(int row, int seat) {
+            this.row = row;
+            this.seat = seat;
+        }
+
+        public static BoardingPass fromString(String line) {
+            String binaryLine = line.substring(0,7);
+            String binaryString = binaryLine.replaceAll("F","0").replaceAll("B","1");
+            int row = Integer.parseInt(binaryString,2);
+            String binaryEnd= line.substring(7);
+            String binarySeatString = binaryEnd.replaceAll("L","0").replaceAll("R","1");
+            int seat = Integer.parseInt(binarySeatString,2);
+            return new BoardingPass(row,seat);
+        }
+    }
+}

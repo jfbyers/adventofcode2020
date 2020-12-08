6a669c8a36d8f0638b73be75c9ea5f01ce1c79e9
Eduardo Aguado <eduardo.aguado@ing.com>
2020-12-08 14:24:49 +0100

    Day 8 a little bit more functional

--- src/com/jfbyers/adventofcode/Day8.java
+++ src/com/jfbyers/adventofcode/Day8.java
@@ -1,8 +1,14 @@
 package com.jfbyers.adventofcode;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day8 {
 
@@ -10,90 +16,81 @@ public class Day8 {
 
     public static void main(String[] args) {
         //Part 1
-        Program p = new Program() ;
-        if (!p.execute()){
-            System.out.println("Infinite loop at program. Accumulator value "+p.accumulator );
+        Program p = new Program();
+        if (!p.execute()) {
+            System.out.println("Infinite loop at program. Accumulator value " + p.accumulator);
         }
 
         //Part 2
         List<Integer> jmpIndexes = getOperandsIndexes(Operand.JMP);
         List<Integer> noopIndexes = getOperandsIndexes(Operand.NOP);
 
-        for (Integer instructionIndexToChange: jmpIndexes){
+        for (Integer instructionIndexToChange : jmpIndexes) {
             Program p1 = new Program();
-            p1.execute(instructionIndexToChange,Operand.NOP);
+            p1.execute(instructionIndexToChange, Operand.NOP);
         }
 
-        for (Integer instructionIndexToChange: noopIndexes){
+        for (Integer instructionIndexToChange : noopIndexes) {
             Program p2 = new Program();
-            p2.execute(instructionIndexToChange,Operand.JMP);
+            p2.execute(instructionIndexToChange, Operand.JMP);
         }
     }
 
     private static List<Integer> getOperandsIndexes(Operand op) {
-        List<Integer> result = new ArrayList<>();
-        for (Instruction i : getInstructions()){
-            if (i.op.equals(op)){
-                result.add(i.index);
-            }
-        }
-        return result;
+        return getInstructions().stream()
+                .filter(i -> i.op.equals(op))
+                .map(instruction -> instruction.index)
+                .collect(Collectors.toList());
     }
 
     private static List<Instruction> getInstructions() {
-        final File file = new File("inputDay8.txt");
-        final List<Instruction> instructionList = new ArrayList<>();
-        try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
-            String line;
-            Integer index = 0;
-            while ((line = br.readLine()) != null) {
-                Instruction i = Instruction.fromString(line, index);
-                instructionList.add(i);
-                index++;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        try (Stream<String> stream = Files.lines(Paths.get("inputDay8.txt"))) {
+            final AtomicInteger index = new AtomicInteger(0);
+            return stream.map(line -> Instruction.fromString(line, index.incrementAndGet())).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return instructionList;
+        return Collections.emptyList();
     }
 
     private static class Program {
         private final List<Instruction> instructions;
+        private final List<Integer> executedInstructions = new ArrayList<>();
+
         private Integer accumulator = 0;
         private int nextInstruction = 0;
-        private List<Integer> memory = new ArrayList<>();
 
         public Program() {
             this.instructions = getInstructions();
         }
 
         public boolean execute() {
-            return execute(null,null);
+            return execute(null, null);
         }
 
-        public boolean execute(Integer instructionIndexToChange,Operand opToChange) {
+        public boolean execute(Integer instructionIndexToChange, Operand opToChange) {
             boolean stop = false;
 
-            while (!stop && !(nextInstruction == this.instructions.size())) {
+            int instructionsLength = this.instructions.size();
+            while (!stop && !(nextInstruction == instructionsLength)) {
                 Instruction ins = this.instructions.get(nextInstruction);
-                if (ins.index.equals(instructionIndexToChange)){
+                if (ins.index.equals(instructionIndexToChange)) {
                     ins.op = opToChange;
                 }
                 stop = executeInstruction(ins);
             }
-            if (nextInstruction == this.instructions.size()) {
-                System.out.println("Program finished at accumulator = " + accumulator + ". Instruction changed to a "+ opToChange+ " at index "+instructionIndexToChange);
+            if (nextInstruction == instructionsLength) {
+                System.out.println("Program finished at accumulator = " + accumulator + ". Instruction changed to a " + opToChange + " at index " + instructionIndexToChange);
                 return true;
             }
             return false;
         }
+
         private boolean executeInstruction(Instruction i) {
-            if (memory.contains(nextInstruction)) {
+            if (executedInstructions.contains(nextInstruction)) {
                 return true;
             }
-            memory.add(nextInstruction);
+            executedInstructions.add(nextInstruction);
 
             switch (i.op) {
                 case ACC:
@@ -123,9 +120,9 @@ public class Day8 {
         }
 
         public static Instruction fromString(String instructionLine, Integer index) {
-            String[] opAndOffeset = instructionLine.split(" ");
-            Operand op = Operand.valueOf(opAndOffeset[0].toUpperCase());
-            Integer offset = Integer.valueOf(opAndOffeset[1]);
+            String[] opAndOffset = instructionLine.split(" ");
+            Operand op = Operand.valueOf(opAndOffset[0].toUpperCase());
+            Integer offset = Integer.valueOf(opAndOffset[1]);
             return new Instruction(op, offset, index);
         }
     }
